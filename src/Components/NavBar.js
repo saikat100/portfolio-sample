5701aab3da225af54745144d061a0aa9239e268c
user <user@users-MacBook-Pro-2.local>
2022-03-03 01:23:36 -0800

    blog page done

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -7,7 +7,6 @@ const NavBar = () => {
 				<div className="container mx-auto flex justify-between">
 					<nav className="flex">
 						<NavLink
-							activeClassName="text-white"
 							className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest"
 							to="/"
 						>
